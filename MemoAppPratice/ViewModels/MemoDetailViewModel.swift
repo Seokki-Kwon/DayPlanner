357f231add06ff5098bb36fa8dbdb0e5df1106db
Seokki-Kwon <gkwon1120@icloud.com>
2024-06-21 21:24:50 +0900

    Fix: MemoDetailViewModel에서 Memo수정시 필드별로 BehaviorSubject로 분리

--- MemoAppPratice/ViewModels/MemoDetailViewModel.swift
+++ MemoAppPratice/ViewModels/MemoDetailViewModel.swift
@@ -9,32 +9,71 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class MemoDetailViewModel: MemoViewModelType {
+// input: editButtonStatus,
+// output: editModeChanged, deleteMemo, editMemo
+
+// editButtonTap -> editModeChanged
+final class MemoDetailViewModel: MemoViewModelType {
     let memo: Memo
-    lazy var memoSubject = BehaviorRelay<Memo>(value: memo)
-    var editModeSubject = PublishSubject<Bool>()
-    var newMemo: Memo {
-        memoSubject.value
-    }
+    private let bag = DisposeBag()
+    private let editModeChangeSubject = PublishSubject<Bool>()
+    private lazy var titleSubject = BehaviorSubject(value: memo.title)
+    private lazy var contentSubject = BehaviorSubject(value: memo.content)
     
     init(memo: Memo, storage: MemoStorageType) {
         self.memo = memo
-        super.init(storage: storage)        
+        super.init(storage: storage)
+    }
+}
+
+extension MemoDetailViewModel: ViewModelType {
+    // input
+    struct Input {
+        let editButtonTap: ControlEvent<Void>
+        let inputTitle: ControlProperty<String>
+        let inputContent: ControlProperty<String>
     }
     
-    func performUpdate() {
-        storage.updateMemo(memo: memoSubject.value)
+    // output
+    struct Output {
+        let editModeChanged: Driver<Bool>
+        let outputTitle: Driver<String>
+        let outputContent: Driver<String>
     }
     
-    func updateContent(content: String) {
-        memoSubject.accept(Memo(id: newMemo.id, title: newMemo.title, content: content))
+    func transform(input: Input) -> Output {
+        input.editButtonTap
+            .scan(false) { last, new in !last}
+            .bind(to: editModeChangeSubject)
+            .disposed(by: bag)
+        
+        // inputTitle, inputContent를 memoSubject값에 바인딩 해야한다.
+        input.inputTitle
+            .changed
+            .bind(to: titleSubject)
+            .disposed(by: bag)
+        
+        input.inputContent
+            .changed
+            .bind(to: contentSubject)
+            .disposed(by: bag)
+        
+        editModeChangeSubject
+            .filter { !$0 }
+            .subscribe { [weak self] _ in self?.performUpdate() }
+            .disposed(by: bag)
+        
+        return Output(editModeChanged: editModeChangeSubject.asDriver(onErrorJustReturn: false),
+                      outputTitle: titleSubject.asDriver(onErrorJustReturn: "Some Title"),
+                      outputContent: contentSubject.asDriver(onErrorJustReturn: "Some Contents")
+        )
     }
     
-    func updateTitle(title: String) {
-        memoSubject.accept(Memo(id: newMemo.id, title: title, content: newMemo.content))
-    }
-    
-    func performDelete() {
-        storage.deleteMemo(memo: newMemo)
+    func performUpdate() {        
+        Observable.zip(titleSubject, contentSubject)
+            .map { [weak self] in
+                 Memo(id: self!.memo.id, title: $0.0, content: $0.1)}
+            .subscribe { self.storage.updateMemo(memo: $0) }
+            .disposed(by: bag)
     }
 }
--- MemoAppPratice/Views/MemoDetailViewController.swift
+++ MemoAppPratice/Views/MemoDetailViewController.swift
@@ -9,77 +9,44 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class MemoDetailViewController: UIViewController, BindableType {
+final class MemoDetailViewController: UIViewController, BindableType {
     var viewModel: MemoDetailViewModel!
-    let bag = DisposeBag()
+    private let bag = DisposeBag()
     
-
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentLabel: UITextView!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
     
     func bindViewModel() {
-        // 변경된 메모내용 -> UI에 바인딩
-        viewModel.memoSubject
-            .withUnretained(self)
-            .subscribe { (vc, memo) in
-                vc.titleTextField.text = memo.title
-                vc.contentLabel.text = memo.content
-            }
+        let input = MemoDetailViewModel.Input(editButtonTap: editButton.rx.tap,
+                                              inputTitle: titleTextField.rx.text.orEmpty,
+                                              inputContent: contentLabel.rx.text.orEmpty)        
+            
+        let output = viewModel.transform(input: input)
+        
+        output.outputTitle
+            .drive(titleTextField.rx.text)
             .disposed(by: bag)
         
-        // 내용입력 -> BehaviorRelay 업데이트
-        contentLabel.rx.text.orEmpty
-            .withUnretained(self)
-            .subscribe(onNext: { (vc, event) in
-                vc.viewModel.updateContent(content: event)
-            })
+        output.outputContent
+            .drive(contentLabel.rx.text)
+            .disposed(by: bag)
+                
+        output.editModeChanged
+            .drive(contentLabel.rx.isUserInteractionEnabled)
             .disposed(by: bag)
         
-        titleTextField.rx.text.orEmpty
-            .withUnretained(self)
-            .subscribe { (vc, event) in
-                vc.viewModel.updateTitle(title: event)
-            }
-            .disposed(by: bag)
-        
-        // editMode에 따라서 isUserIteractionEable, text 값 변화
-        viewModel.editModeSubject
-            .bind(to: contentLabel.rx.isUserInteractionEnabled)
-            .disposed(by: bag)
-        
-        viewModel.editModeSubject
-            .bind(to: titleTextField.rx.isEnabled)
-            .disposed(by: bag)
-        
-        // 수정이 완료되면 -> 변경된 내용을 업데이트
-        // 수정을 하려고하면 -> 텍스트 변경 값바디인딩
-        viewModel.editModeSubject
-            .withUnretained(self)
-            .do(onNext: { (vc, editMode) in
-                if !editMode {
-                    vc.viewModel.performUpdate()
-                }
-            })
-            .map { $1 ? "수정 완료" : "메모 수정"}
+        output.editModeChanged
+            .asObservable()
+            .map { $0 ? "편집 완료" : "메모 편집"}
             .bind(to: editButton.rx.title())
             .disposed(by: bag)
         
-        // editMode 바인딩
-        editButton.rx.tap
-            .scan(false) { lastState, newState in !lastState }
-            .bind(to: viewModel.editModeSubject)
+        output.editModeChanged
+            .drive(titleTextField.rx.isEnabled)
             .disposed(by: bag)
         
-        deleteButton.rx.tap
-            .withUnretained(self)
-            .subscribe(onNext: {(vc, _) in
-                vc.viewModel.performDelete()
-                vc.navigationController?.popViewController(animated: true)
-            })
-            .disposed(by: bag)
-            
     }
     
     override func viewDidLoad() {
