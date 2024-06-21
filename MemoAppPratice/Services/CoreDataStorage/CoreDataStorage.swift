e2afe06540aa17fd592b167a943cc508346e392c
Seokki-Kwon <rnjstjrrl12@gmail.com>
2024-06-22 01:07:49 +0900

    Fix: 저장소 변경 Memory -> CoreData

---
+++ MemoAppPratice/Services/CoreDataStorage/CoreDataStorage.swift
@@ -0,0 +1,92 @@
+//
+//  CoreDataStorage.swift
+//  MemoAppPratice
+//
+//  Created by 석기권 on 6/21/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import CoreData
+
+final class CoreDataStorage: MemoStorageType {
+    let modelName: String
+    private let store = BehaviorSubject<[Memo]>(value: [])
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    // container
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // context
+    private var mainContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    /// 메모 추가 Upsert
+    @discardableResult
+    func updateMemo(memo: Memo) -> RxSwift.Observable<Memo> {
+        guard let entity = NSEntityDescription.entity(forEntityName: "Memo", in: mainContext) else {
+            return Observable.empty()
+        }
+        
+        let memoObject = NSManagedObject(entity: entity, insertInto: mainContext)
+        memoObject.setValue(memo.id, forKey: "id")
+        memoObject.setValue(memo.title, forKey: "title")
+        memoObject.setValue(memo.content, forKey: "content")
+        
+        do {
+            _ = try mainContext.save()
+            let currentData = try store.value()
+            store.onNext(currentData + [memo])
+            return Observable.just(memo)
+        } catch {
+            return Observable.error(error)
+        }
+    }
+    
+    /// 메모 삭제
+    @discardableResult
+    func deleteMemo(memo: Memo) -> RxSwift.Observable<Void> {
+       let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
+        fetchRequest.predicate = NSPredicate(format: "id == %@", memo.id as CVarArg)
+        
+        do {
+            if let fetchRequest = try mainContext.fetch(fetchRequest) as? [NSManagedObject],
+               let memoObject = fetchRequest.first {
+                mainContext.delete(memoObject)
+                try mainContext.save()
+                let updated = try store.value().filter { $0.id != memo.id }
+                store.onNext(updated)
+                return Observable.just(())
+            }
+        } catch {
+            return Observable.error(error)
+        }
+        return Observable.just(())
+    }
+    
+    /// 메모 가져오기
+    @discardableResult
+    func fetchMemos() -> RxSwift.Observable<[Memo]> {
+        do {
+            let data = try mainContext.fetch(MemoEntity.fetchRequest())
+            let memoDatas = data.map { Memo(id: $0.id!, title: $0.title!, content: $0.content! )}
+            store.onNext(memoDatas)
+            return store
+        } catch {
+            return Observable.error(error)
+        }
+    }
+}
