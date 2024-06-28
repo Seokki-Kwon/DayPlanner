6587522badc8a6128f43368f955150a9b521a0d9
Seokki-Kwon <gkwon1120@icloud.com>
2024-06-28 18:48:53 +0900

    Feat: 커스텀캘린더뷰

---
+++ DayPlanner/UI/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  DayPlanner
+//
+//  Created by 석기권 on 6/28/24.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    static let reuseIdentifier = "calCell"
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
--- DayPlanner/Views/CalenderViewController.swift
+++ DayPlanner/Views/CalenderViewController.swift
@@ -8,10 +8,8 @@
 import UIKit
 
 class CalenderViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-    
 }
